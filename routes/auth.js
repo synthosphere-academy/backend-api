7c153f62ed265b65c1422573e0401c35da717bf0
Sourav Basak <149062402+BrandWise-Tech@users.noreply.github.com>
2024-06-10 11:51:36 +0530

    route/auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login } = require('../controller/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
